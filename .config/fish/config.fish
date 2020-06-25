dc26ffd7d4e23617f3f2dabf70819f84b47e3849
Simon Zeng <contact@simonzeng.com>
2020-06-24 21:45:43 -0400

    Add qutebrowser

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 # fish_vi_key_bindings
 set PATH /home/kronicmage/bin /home/kronicmage/.local/bin /home/kronicmage/.cabal/bin /home/kronicmage/.ghcup/bin $PATH
+set NODE_PATH /usr/lib/node_modules $NODE_PATH
 set -gx EDITOR nvim
 set -gx PAGER nvim -R
 set -gx MANPAGER "nvim -R -c 'set ft=man' -"
---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,26 @@
+config.source('qutewal.py')
+c.url.start_pages = ["file:///home/kronicmage/repos/startpage/index.html"]
+c.url.default_page = "file:///home/kronicmage/repos/startpage/index.html"
+c.url.searchengines = {"DEFAULT": "https://google.ca/search?q={}"}
+c.tabs.background = True
+c.editor.command = ['/usr/bin/kitty', '/usr/bin/nvim', '{}']
+c.tabs.show = 'multiple'
+c.aliases = {
+    "q": "close",
+    "qa": "quit",
+    "w": "session-save",
+    "wq": "quit --save",
+    "wqa": "quit --save",
+    "pass": "spawn --userscript qute-lastpass",
+    "getbib": "spawn --userscript getbib",
+    "json": "spawn --userscript format_json",
+    "read": "spawn --userscript readability",
+    "read_extern": "open https://outline.com/{url}",
+    "dl": "spawn --userscript open_download"
+}
+c.colors.webpage.bg = ""
+# c.content.user_stylesheets = ['~/.config/qutebrowser/css/gruvbox-all-sites.css'] 
+config.bind('<Ctrl-R>', 'config-cycle content.user_stylesheets "~/.config/qutebrowser/css/apprentice-all-sites.css" "~/.config/qutebrowser/css/darculized-all-sites.css" "~/.config/qutebrowser/css/gruvbox-all-sites.css" "~/.config/qutebrowser/css/solarized-dark-all-sites.css" "~/.config/qutebrowser/css/solarized-light-all-sites.css" ""')
+config.bind('cn', 'config-cycle content.user_stylesheets "~/.config/qutebrowser/css/gruvbox-all-sites.css" ""')
+
+
